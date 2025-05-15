25595a0bae47bf06bbe1d6835a89c781bd4448b8
Kelvin Wekesa <106185953+WekesaKelvin@users.noreply.github.com>
2025-05-15 21:57:46 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,7 @@ import { ProductFormComponent } from './pages/product-form/product-form.componen
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'list', component: ProductListComponent },
-
-  // For adding a new product: /form
   { path: 'form', component: ProductFormComponent },
-
-  // For editing an existing product: /form/123
   { path: 'form/:id', component: ProductFormComponent },
-
   { path: '**', redirectTo: '' }
 ];
